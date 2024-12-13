ecd49cc31d2cf81836fa9716da65467255f9e95f
user <user@user.ru>
2024-12-13 10:56:09 +0400

    sigma boy

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,12 @@
+﻿// калкулатор
+Console.WriteLine("Введите кол-во дней на которое положите деньги");
+double dayInMonth = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите сумму вклада");
+double vklad = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите процентную ставку в десятичной дроби (15% == 0,15%)");
+double procent = Convert.ToDouble(Console.ReadLine());
+int day = 365;
+double pribil = vklad * Math.Pow((1 + procent / day), dayInMonth);
+ pribil = Math.Floor(pribil);
+Console.WriteLine("Общая прибыль: " + pribil);
+ Task.Delay(10000).Wait();
